520f710ef985f5efd47ec0748b18dc8d003fb879
danjocutler <danjocutler@gmail.com>
2015-03-29 15:40:19 +0100

    Board can be created using a string of chars

---
+++ ConwayGameOfLife/BoardTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ConwayGameOfLife
+{
+    [TestFixture]
+    public class BoardTests
+    {
+        [Test]
+        public void Board_Given2x2Board_CreatesBoard()
+        {
+            var board = new GameOfLifeBoard(@"
+##
+##".Trim());
+
+            Assert.That(board.Width, Is.EqualTo(2));
+            Assert.That(board.Height, Is.EqualTo(2));
+
+        }
+
+        [Test]
+        public void Board_Given5x5Board_CreatesBoard()
+        {
+            var board = new GameOfLifeBoard(@"
+#####
+#####
+#####
+#####
+#####".Trim());
+
+            Assert.That(board.Width, Is.EqualTo(5));
+            Assert.That(board.Height, Is.EqualTo(5));
+
+        }
+    }
+}
---
+++ ConwayGameOfLife/GameOfLifeBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConwayGameOfLife
+{
+    class GameOfLifeBoard
+    {
+        private List<string> _board;
+
+        public int Width { get { return _board.Max(width => width.Length); } }
+        public int Height { get { return _board.Count; } }
+
+        public GameOfLifeBoard(string board)
+        {
+            _board = board.Split(new [] { Environment.NewLine }, StringSplitOptions.None).ToList();
+        }
+
+
+    }
+}
